2cf9d6b8ee23a1ca6ba1699f9a23d0bc714b36db
revalindaph <254107060053@student.polinema.ac.id>
2026-09-11 15:12:23 +0700

    Jobsheet 02 Percobaan 2 Barang

---
+++ Jobsheet02/Percobaan2/Barang23.java
@@ -0,0 +1,20 @@
+package Jobsheet02.Percobaan2;
+
+public class Barang23 {
+    public String kode;
+    public String nama;
+    public double hargaKotor;
+    public double diskon;
+
+    public double getHargaBersih() {
+        return hargaKotor - (diskon * hargaKotor);
+    }
+
+    public void displayInfo() {
+        System.out.println("Kode         : " + kode);
+        System.out.println("Nama         : " + nama);
+        System.out.println("Harga Kotor  : " + hargaKotor);
+        System.out.println("Diskon       : " + diskon);
+        System.out.println("Harga Bersih : " + getHargaBersih());
+    }
+}
---
+++ Jobsheet02/Percobaan2/BarangDemo23.java
@@ -0,0 +1,13 @@
+package Jobsheet02.Percobaan2;
+
+public class BarangDemo23 {
+    public static void main(String[] args) {
+        Barang23 barang1 = new Barang23();
+        barang1.kode = "ATK01";
+        barang1.nama = "Bolpoin Pilot Hitam";
+        barang1.hargaKotor = 3500;
+        barang1.diskon = 0.1;
+
+        barang1.displayInfo();
+    }
+}
