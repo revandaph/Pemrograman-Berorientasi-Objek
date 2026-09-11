de5260fe18b8aede3d9a986c2759c95f5230aaa3
revalindaph <254107060053@student.polinema.ac.id>
2026-09-11 22:13:27 +0700

    Tugas Teori 02 Tambah constructor default dan berparameter pada Laptop23

--- Teori02/ClassAndObject/Laptop23.java
+++ Teori02/ClassAndObject/Laptop23.java
@@ -6,8 +6,22 @@ public class Laptop23 {
     public int ram;
     public int persentaseBaterai;
 
+    public Laptop23() {
+        this.merk = "Acer Aspire";
+        this.processor = "Intel Core i3";
+        this.ram = 8;
+        this.persentaseBaterai = 100;
+    }
+
+    public Laptop23(String merk, String processor, int ram, int persentaseBaterai) {
+        this.merk = merk;
+        this.processor = processor;
+        this.ram = ram;
+        this.persentaseBaterai = persentaseBaterai;
+    }
+
     public void nyalakan() {
-        System.out.println("Sistem booting... Laptop " + merk + " siap digunakan.");
+        System.out.println("Sistem booting... Laptop " + merk + " (" + processor + ") siap digunakan.");
     }
 
     public void bukaProgram(String namaProgram) {
